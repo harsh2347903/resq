ced27797646d9454f42890e631cb55ead11a302d
HarshalMogare0607 <harshalmogare58@gmail.com>
2026-09-09 13:55:52 +0530

    build: update cssTarget in vite.config.js for modern standard backdrop-filter

--- resq_vnext/vite.config.js
+++ resq_vnext/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022',
+    cssTarget: 'chrome100'
+  },
   server: {
     port: 5173,
     proxy: {
